2fb794ccd8f70f8196904f6a57a1f35db90eb647
sugare <30733705@qq.com>
2017-12-02 17:19:46 +0800

    dns

---
+++ Images.sh
@@ -0,0 +1,36 @@
+# quay.io/coreos/etcd:v3.2.4
+# gcr.io/google_containers/kubernetes-dashboard-amd64:v1.6.3
+# gcr.io/google_containers/cluster-proportional-autoscaler-amd64:1.1.1
+# gcr.io/google_containers/elasticsearch:v2.4.1
+# gcr.io/google_containers/fluentd-elasticsearch:1.22
+# gcr.io/google_containers/kibana:v4.6.1
+# gcr.io/google_containers/pause-amd64:3.0
+# quay.io/calico/routereflector:v0.4.0
+
+quay.io/coreos/flannel:v0.9.1
+quay.io/coreos/flannel-cni:v0.3.0
+
+quay.io/calico/ctl:v1.6.1
+quay.io/calico/node:v2.6.2
+quay.io/calico/cni:v1.11.0
+quay.io/calico/kube-controllers:v1.0.0
+
+quay.io/l23network/k8s-netchecker-agent:v1.0
+quay.io/l23network/k8s-netchecker-server:v1.0
+
+
+quay.io/coreos/hyperkube:v1.8.3_coreos.0
+
+xueshanf/install-socat
+
+weaveworks/weave-kube:2.0.5
+weaveworks/weave-npc:2.0.5
+contiv/netplugin:1.1.7
+contiv/auth_proxy:1.1.7
+
+gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.7
+gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.7
+gcr.io/google_containers/k8s-dns-sidecar-amd64:1.14.7
+
+lachlanevenson/k8s-helm:v2.7.2
+gcr.io/kubernetes-helm/tiller:v2.7.2
